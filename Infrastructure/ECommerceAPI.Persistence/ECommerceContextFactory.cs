14e90d3b549913c0a472e48e81c96b38cdf345c9
bayra <bayra@DESKTOP-EI3EV3V>
2023-01-04 06:27:37 +0400

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/ECommerceContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceAPI.Persistence.Configurations;
+using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public class ECommerceContextFactory : IDesignTimeDbContextFactory<ECommerceAPIDbContext>
+    {
+        public ECommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder optionsBuilder = new();
+            optionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(optionsBuilder.Options);
+        }
+    }
+}
